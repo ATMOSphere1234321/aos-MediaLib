4355f8fe82611c24a06656fadfce5a6dbcbab27d
matrix <austin.316scsa@yahoo.com>
2025-03-15 01:25:07 +0300

    Redesigned Global Resume Button

--- src/com/archos/mediacenter/utils/GlobalResumeView.java
+++ src/com/archos/mediacenter/utils/GlobalResumeView.java
@@ -18,6 +18,14 @@ import android.animation.Animator.AnimatorListener;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.BitmapShader;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.util.AttributeSet;
 import android.view.ViewPropertyAnimator;
 import android.widget.RelativeLayout;
@@ -40,7 +48,7 @@ public class GlobalResumeView extends RelativeLayout {
             resizeImage(mImage, w, h);
             mImage = null;
         }
-   }
+    }
 
     public void setImage(Bitmap bm) {
         if (bm == null) {
@@ -89,9 +97,28 @@ public class GlobalResumeView extends RelativeLayout {
             sbm.recycle();
         }
 
-        setBackground(new BitmapDrawable(getResources(), cbm));
-    }
+        // Create a BitmapShader to apply the image texture
+        BitmapShader shader = new BitmapShader(cbm, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
 
+        // Create a ShapeDrawable with rounded corners
+        float px = 161;
+        float density = getResources().getDisplayMetrics().density;
+        float radius = px / density;
+
+        //float radius = 100f;  // Adjust corner radius as needed
+        RoundRectShape roundRectShape = new RoundRectShape(new float[] { radius, radius, radius, radius, radius, radius, radius, radius }, null, null);
+        ShapeDrawable shapeDrawable = new ShapeDrawable(roundRectShape);
+        shapeDrawable.getPaint().setShader(shader);
+        shapeDrawable.getPaint().setAntiAlias(true);
+
+        // Create a Bitmap from the shapeDrawable
+        Bitmap roundedBitmap = Bitmap.createBitmap(dstWidth, dstHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(roundedBitmap);
+        shapeDrawable.setBounds(0, 0, dstWidth, dstHeight);
+        shapeDrawable.draw(canvas);
+
+        setBackground(new BitmapDrawable(getResources(), roundedBitmap));
+    }
 
     public void launchOpenAnimation(AnimatorListener listener) {
         ViewPropertyAnimator a = animate();
