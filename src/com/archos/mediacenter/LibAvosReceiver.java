b9ab499195c072cb3510d034c8fcc50a7f1a627b
Courville Software <software@courville.org>
2025-05-09 16:39:51 +0200

    Fixes https://github.com/nova-video-player/aos-AVP/issues/1444

--- src/com/archos/mediacenter/LibAvosReceiver.java
+++ src/com/archos/mediacenter/LibAvosReceiver.java
@@ -50,67 +50,72 @@ public class LibAvosReceiver extends BroadcastReceiver {
         } else if (action.equals("com.archos.mediacenter.DEBUG_SCAN")) {
             if (intent.getData() != null) Log.d("IMetadataRetriever", intent.getData().getPath());
             final IMediaMetadataRetriever retriever = MediaFactory.createMetadataRetriever(context);
-            retriever.setDataSource(context, intent.getData());
-            final MediaMetadata metadata = retriever.getMediaMetadata();
+            try {
+                retriever.setDataSource(context, intent.getData());
+                final MediaMetadata metadata = retriever.getMediaMetadata();
 
-            for (int i = 0; i < 2; ++i) {
-                RetrieverDelegate r;
-                if (i == 0) {
-                    Log.d("IMetadataRetriever", "IMediaMetadataRetriever.extractMetadata():");
-                    r = new RetrieverDelegate() {
-                        public String get(int key) {
-                            return retriever.extractMetadata(key);
-                        }
-                    };
-                } else {
-                    Log.d("IMetadataRetriever", "MediaMetadata.getString():");
-                    r = new RetrieverDelegate() {
-                        public String get(int key) {
-                            if (metadata.has(key))
-                                return metadata.getString(key);
-                            else
-                                return null;
-                        }
-                    };
+                for (int i = 0; i < 2; ++i) {
+                    RetrieverDelegate r;
+                    if (i == 0) {
+                        Log.d("IMetadataRetriever", "IMediaMetadataRetriever.extractMetadata():");
+                        r = new RetrieverDelegate() {
+                            public String get(int key) {
+                                return retriever.extractMetadata(key);
+                            }
+                        };
+                    } else {
+                        Log.d("IMetadataRetriever", "MediaMetadata.getString():");
+                        r = new RetrieverDelegate() {
+                            public String get(int key) {
+                                if (metadata.has(key))
+                                    return metadata.getString(key);
+                                else
+                                    return null;
+                            }
+                        };
+                    }
+
+                    displayKey(r, "METADATA_KEY_CD_TRACK_NUMBER", IMediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
+                    displayKey(r, "METADATA_KEY_ALBUM", IMediaMetadataRetriever.METADATA_KEY_ALBUM);
+                    displayKey(r, "METADATA_KEY_ARTIST", IMediaMetadataRetriever.METADATA_KEY_ARTIST);
+                    displayKey(r, "METADATA_KEY_AUTHOR", IMediaMetadataRetriever.METADATA_KEY_AUTHOR);
+                    displayKey(r, "METADATA_KEY_COMPOSER", IMediaMetadataRetriever.METADATA_KEY_COMPOSER);
+                    displayKey(r, "METADATA_KEY_DATE", IMediaMetadataRetriever.METADATA_KEY_DATE);
+                    displayKey(r, "METADATA_KEY_GENRE", IMediaMetadataRetriever.METADATA_KEY_GENRE);
+                    displayKey(r, "METADATA_KEY_TITLE", IMediaMetadataRetriever.METADATA_KEY_TITLE);
+                    displayKey(r, "METADATA_KEY_YEAR", IMediaMetadataRetriever.METADATA_KEY_YEAR);
+                    displayKey(r, "METADATA_KEY_DURATION", IMediaMetadataRetriever.METADATA_KEY_DURATION);
+                    displayKey(r, "METADATA_KEY_NUM_TRACKS", IMediaMetadataRetriever.METADATA_KEY_NUM_TRACKS);
+                    displayKey(r, "METADATA_KEY_WRITER", IMediaMetadataRetriever.METADATA_KEY_WRITER);
+                    displayKey(r, "METADATA_KEY_MIMETYPE", IMediaMetadataRetriever.METADATA_KEY_MIMETYPE);
+                    displayKey(r, "METADATA_KEY_ALBUMARTIST", IMediaMetadataRetriever.METADATA_KEY_ALBUMARTIST);
+                    displayKey(r, "METADATA_KEY_DISC_NUMBER", IMediaMetadataRetriever.METADATA_KEY_DISC_NUMBER);
+                    displayKey(r, "METADATA_KEY_COMPILATION", IMediaMetadataRetriever.METADATA_KEY_COMPILATION);
+                    displayKey(r, "METADATA_KEY_HAS_AUDIO", IMediaMetadataRetriever.METADATA_KEY_HAS_AUDIO);
+                    displayKey(r, "METADATA_KEY_HAS_VIDEO", IMediaMetadataRetriever.METADATA_KEY_HAS_VIDEO);
+                    displayKey(r, "METADATA_KEY_VIDEO_WIDTH", IMediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+                    displayKey(r, "METADATA_KEY_VIDEO_HEIGHT", IMediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+                    displayKey(r, "METADATA_KEY_BITRATE", IMediaMetadataRetriever.METADATA_KEY_BITRATE);
+                    displayKey(r, "METADATA_KEY_TIMED_TEXT_LANGUAGES", IMediaMetadataRetriever.METADATA_KEY_TIMED_TEXT_LANGUAGES);
+                    displayKey(r, "METADATA_KEY_IS_DRM", IMediaMetadataRetriever.METADATA_KEY_IS_DRM);
+                    displayKey(r, "METADATA_KEY_LOCATION", IMediaMetadataRetriever.METADATA_KEY_LOCATION);
+                    displayKey(r, "METADATA_KEY_SAMPLE_RATE", IMediaMetadataRetriever.METADATA_KEY_SAMPLE_RATE);
+                    displayKey(r, "METADATA_KEY_NUMBER_OF_CHANNELS", IMediaMetadataRetriever.METADATA_KEY_NUMBER_OF_CHANNELS);
+                    displayKey(r, "METADATA_KEY_AUDIO_WAVE_CODEC", IMediaMetadataRetriever.METADATA_KEY_AUDIO_WAVE_CODEC);
+                    displayKey(r, "METADATA_KEY_AUDIO_BITRATE", IMediaMetadataRetriever.METADATA_KEY_AUDIO_BITRATE);
+                    displayKey(r, "METADATA_KEY_VIDEO_FOURCC_CODEC", IMediaMetadataRetriever.METADATA_KEY_VIDEO_FOURCC_CODEC);
+                    displayKey(r, "METADATA_KEY_VIDEO_BITRATE", IMediaMetadataRetriever.METADATA_KEY_VIDEO_BITRATE);
+                    displayKey(r, "METADATA_KEY_FRAMES_PER_THOUSAND_SECONDS", IMediaMetadataRetriever.METADATA_KEY_FRAMES_PER_THOUSAND_SECONDS);
+                    displayKey(r, "METADATA_KEY_ENCODING_PROFILE", IMediaMetadataRetriever.METADATA_KEY_ENCODING_PROFILE);
+                    displayKey(r, "METADATA_KEY_NB_VIDEO_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_VIDEO_TRACK);
+                    displayKey(r, "METADATA_KEY_NB_AUDIO_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_AUDIO_TRACK);
+                    displayKey(r, "METADATA_KEY_NB_SUBTITLE_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_SUBTITLE_TRACK);
                 }
-
-                displayKey(r, "METADATA_KEY_CD_TRACK_NUMBER", IMediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
-                displayKey(r, "METADATA_KEY_ALBUM", IMediaMetadataRetriever.METADATA_KEY_ALBUM);
-                displayKey(r, "METADATA_KEY_ARTIST", IMediaMetadataRetriever.METADATA_KEY_ARTIST);
-                displayKey(r, "METADATA_KEY_AUTHOR", IMediaMetadataRetriever.METADATA_KEY_AUTHOR);
-                displayKey(r, "METADATA_KEY_COMPOSER", IMediaMetadataRetriever.METADATA_KEY_COMPOSER);
-                displayKey(r, "METADATA_KEY_DATE", IMediaMetadataRetriever.METADATA_KEY_DATE);
-                displayKey(r, "METADATA_KEY_GENRE", IMediaMetadataRetriever.METADATA_KEY_GENRE);
-                displayKey(r, "METADATA_KEY_TITLE", IMediaMetadataRetriever.METADATA_KEY_TITLE);
-                displayKey(r, "METADATA_KEY_YEAR", IMediaMetadataRetriever.METADATA_KEY_YEAR);
-                displayKey(r, "METADATA_KEY_DURATION", IMediaMetadataRetriever.METADATA_KEY_DURATION);
-                displayKey(r, "METADATA_KEY_NUM_TRACKS", IMediaMetadataRetriever.METADATA_KEY_NUM_TRACKS);
-                displayKey(r, "METADATA_KEY_WRITER", IMediaMetadataRetriever.METADATA_KEY_WRITER);
-                displayKey(r, "METADATA_KEY_MIMETYPE", IMediaMetadataRetriever.METADATA_KEY_MIMETYPE);
-                displayKey(r, "METADATA_KEY_ALBUMARTIST", IMediaMetadataRetriever.METADATA_KEY_ALBUMARTIST);
-                displayKey(r, "METADATA_KEY_DISC_NUMBER", IMediaMetadataRetriever.METADATA_KEY_DISC_NUMBER);
-                displayKey(r, "METADATA_KEY_COMPILATION", IMediaMetadataRetriever.METADATA_KEY_COMPILATION);
-                displayKey(r, "METADATA_KEY_HAS_AUDIO", IMediaMetadataRetriever.METADATA_KEY_HAS_AUDIO);
-                displayKey(r, "METADATA_KEY_HAS_VIDEO", IMediaMetadataRetriever.METADATA_KEY_HAS_VIDEO);
-                displayKey(r, "METADATA_KEY_VIDEO_WIDTH", IMediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
-                displayKey(r, "METADATA_KEY_VIDEO_HEIGHT", IMediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
-                displayKey(r, "METADATA_KEY_BITRATE", IMediaMetadataRetriever.METADATA_KEY_BITRATE);
-                displayKey(r, "METADATA_KEY_TIMED_TEXT_LANGUAGES", IMediaMetadataRetriever.METADATA_KEY_TIMED_TEXT_LANGUAGES);
-                displayKey(r, "METADATA_KEY_IS_DRM", IMediaMetadataRetriever.METADATA_KEY_IS_DRM);
-                displayKey(r, "METADATA_KEY_LOCATION", IMediaMetadataRetriever.METADATA_KEY_LOCATION);
-                displayKey(r, "METADATA_KEY_SAMPLE_RATE", IMediaMetadataRetriever.METADATA_KEY_SAMPLE_RATE);
-                displayKey(r, "METADATA_KEY_NUMBER_OF_CHANNELS", IMediaMetadataRetriever.METADATA_KEY_NUMBER_OF_CHANNELS);
-                displayKey(r, "METADATA_KEY_AUDIO_WAVE_CODEC", IMediaMetadataRetriever.METADATA_KEY_AUDIO_WAVE_CODEC);
-                displayKey(r, "METADATA_KEY_AUDIO_BITRATE", IMediaMetadataRetriever.METADATA_KEY_AUDIO_BITRATE);
-                displayKey(r, "METADATA_KEY_VIDEO_FOURCC_CODEC", IMediaMetadataRetriever.METADATA_KEY_VIDEO_FOURCC_CODEC);
-                displayKey(r, "METADATA_KEY_VIDEO_BITRATE", IMediaMetadataRetriever.METADATA_KEY_VIDEO_BITRATE);
-                displayKey(r, "METADATA_KEY_FRAMES_PER_THOUSAND_SECONDS", IMediaMetadataRetriever.METADATA_KEY_FRAMES_PER_THOUSAND_SECONDS);
-                displayKey(r, "METADATA_KEY_ENCODING_PROFILE", IMediaMetadataRetriever.METADATA_KEY_ENCODING_PROFILE);
-                displayKey(r, "METADATA_KEY_NB_VIDEO_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_VIDEO_TRACK);
-                displayKey(r, "METADATA_KEY_NB_AUDIO_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_AUDIO_TRACK);
-                displayKey(r, "METADATA_KEY_NB_SUBTITLE_TRACK", IMediaMetadataRetriever.METADATA_KEY_NB_SUBTITLE_TRACK);
+            } catch (IllegalArgumentException e) {
+                Log.e("LibAvosReceiver", "Invalid data source URI", e);
+            } finally {
+                retriever.release();
             }
-            retriever.release();
         }
     }
 }
